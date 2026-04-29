5b129161d8b8f279cfb3ad46a70023dc0fa3a45f
Luciano <luciano.marin02@inacapmail.cl>
2026-04-29 00:30:46 -0400

    cambios

--- src/test/java/com/sumativa1/reserva_cita/controller/CitaControllerTest.java
+++ src/test/java/com/sumativa1/reserva_cita/controller/CitaControllerTest.java
@@ -1,5 +1,30 @@
 package com.sumativa1.reserva_cita.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import com.sumativa1.reserva_cita.service.CitaService;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+
+
+@WebMvcTest(CitaController.class)
 public class CitaControllerTest {
+
+   @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CitaService citaService;
+
+
 
 }
